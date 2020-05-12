14b7687ba7a29653f0a9f9a9414236af0631a323
wanglihui <1769794040@qq.com>
2020-05-12 17:58:55 +0800

    test: 测试

--- config/config.production.js
+++ config/config.production.js
@@ -2,4 +2,5 @@
 
 module.exports = {
   baseURL: 'https://microuag.dgg188.cn/', // 小顶云接口
+  secret: '0753da1791bdae281ef8cb6ec8daacf0', // wap端接口验签密钥
 }
--- config/config.release.js
+++ config/config.release.js
@@ -2,4 +2,5 @@
 
 module.exports = {
   baseURL: 'https://ymicrouag.dgg188.cn/', // 小顶云接口
+  secret: '930def3fd2dea5e3c1370f2ac50816aa', // wap端接口验签密钥
 }
